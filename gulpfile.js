a478fcd87e6d6e3ed1870c3d5962ccb5f36bf662
yeonlisa <tkzw4869@gmail.com>
2022-02-21 18:41:47 +0900

    Purging CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
     return src('shinobi/**/*.scss')
         .pipe(sass())
+        .pipe(purgecss({ content: ['*.html']}))
         .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['shinobi/**/*.scss'], buildStyles)
+    watch(['shinobi/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
