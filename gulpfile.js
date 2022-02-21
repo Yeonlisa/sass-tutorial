63fce8a1e0af6154ed5709e283e3142538131ea9
yeonlisa <tkzw4869@gmail.com>
2022-02-21 18:51:28 +0900

    Customizing the Library

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
-    return src('shinobi/**/*.scss')
+    return src('sass/**/*.scss')
         .pipe(sass())
         .pipe(purgecss({ content: ['*.html']}))
         .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['shinobi/**/*.scss', '*.html'], buildStyles)
+    watch(['sass/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
